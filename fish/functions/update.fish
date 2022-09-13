a3bcc36b9438954a35454d91d91aa886e7e69c17
Tobias von Klipstein <tobias@klpstn.com>
2022-09-13 23:03:20 +0200

    Install mas and fix update task

--- fish/functions/update.fish
+++ fish/functions/update.fish
@@ -1,7 +1,7 @@
 function update --description 'Get OS X Software Updates and update Homebrew, mas, and npm packages'
     sudo softwareupdate -i -a
     brew update
-    brew upgrade --force --all
+    brew upgrade
     brew cleanup
     mas upgrade
     npm install npm -g
